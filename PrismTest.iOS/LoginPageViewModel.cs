0b0b5fe1c60ff4accccd8cd6e6662cfcbef349f9
Luis Pujols <luispujols@hotmail.com>
2021-08-14 20:58:07 -0400

    Added live demo project code for Latino .NET Talk

---
+++ PrismTest.iOS/LoginPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+
+namespace PrismTest.ViewModels
+{
+    public class LoginPageViewModel : BindableBase
+    {
+        public DelegateCommand OnLoginCommand { get; set; }
+        INavigationService _navigationService;
+        IPageDialogService _pageDialogService;
+
+        public string _userName;
+        public string Username
+        {
+            get { return _userName; }
+            set { SetProperty(ref _userName, value); }
+        }
+
+        public string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        public LoginPageViewModel(INavigationService navigationService,
+               IPageDialogService pageDialogService)
+        {
+            _navigationService = navigationService;
+            _pageDialogService = pageDialogService;
+
+            OnLoginCommand = new DelegateCommand(async () => await GoMainPage());
+        }
+
+        private async Task GoMainPage()
+        {
+            if (string.IsNullOrEmpty(Username))
+                await _pageDialogService.DisplayAlertAsync("Error", "Username is required", "Ok");
+            else
+                await _navigationService.NavigateAsync("MainPage");
+            //await _navigationService.NavigateAsync(new Uri($"MainPage"));
+        }
+    }
+}
---
+++ PrismTest/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using PrismTest.ViewModels;
+using PrismTest.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PrismTest
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+        
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync(new System.Uri("/NavigationPage/LoginPage"));
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage>();
+            containerRegistry.RegisterForNavigation<LoginPage,LoginPageViewModel>();
+        }
+
+        //protected override void OnStart()
+        //{
+        //}
+
+        //protected override void OnSleep()
+        //{
+        //}
+
+        //protected override void OnResume()
+        //{
+        //}
+    }
+}
